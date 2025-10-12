c67bff000a043b5f4bc51d87fc06fd2c0cd6b54d
MrNezihBJ <nezihbenjemia15@gmail.com>
2025-10-12 18:45:14 +0100

    dropdown

--- src/app/pages/articles/articles.component.ts
+++ src/app/pages/articles/articles.component.ts
@@ -1,5 +1,4 @@
-import { Component } from "@angular/core";
-import { DefaultInputsComponent } from "../../shared/components/form/form-elements/default-inputs/default-inputs.component";
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnDestroy, Output, ViewChild } from "@angular/core";
 import { ComponentCardComponent } from "../../shared/components/common/component-card/component-card.component";
 import { LabelComponent } from "../../shared/components/form/label/label.component";
 import { CommonModule } from "@angular/common";
@@ -16,4 +15,33 @@ import { InputFieldComponent } from "../../shared/components/form/input/input-fi
   templateUrl: "./articles.component.html",
   styleUrl: "./articles.component.css",
 })
+
+
+export class DropdownComponent implements AfterViewInit, OnDestroy {
+    @Input() className = '';
+    @Input() isOpen = false;
+    @Output() close = new EventEmitter<void>();
+ 
+    @ViewChild('dropdownRef') dropdownRef!: ElementRef<HTMLDivElement>;
+ 
+    private handleClickOutside = (event: MouseEvent) => {
+      if (
+        this.isOpen &&
+        this.dropdownRef &&
+        this.dropdownRef.nativeElement &&
+        !this.dropdownRef.nativeElement.contains(event.target as Node) &&
+        !(event.target as HTMLElement).closest('.dropdown-toggle')
+      ) {
+        this.close.emit();
+      }
+    };
+ 
+    ngAfterViewInit() {
+      document.addEventListener('mousedown', this.handleClickOutside);
+    }
+ 
+    ngOnDestroy() {
+      document.removeEventListener('mousedown', this.handleClickOutside);
+    }
+  }
 export class ArticlesComponent {}
