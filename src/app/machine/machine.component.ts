ced2aa621ba4b846f32e12b1892614072db6bdc9
MrNezihBJ <benjemianezih-glitch>
2025-10-13 19:08:04 +0100

    dropdown in progress

--- src/app/machine/machine.component.ts
+++ src/app/machine/machine.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 import { ComponentCardComponent } from "../shared/components/common/component-card/component-card.component";
 import { InputFieldComponent } from "../shared/components/form/input/input-field.component";
 import { LabelComponent } from "../shared/components/form/label/label.component";
+import { DropdownComponent } from "../shared/components/ui/dropdown/dropdown.component";
 
 @Component({
   selector: "app-machine",
@@ -11,8 +12,23 @@ import { LabelComponent } from "../shared/components/form/label/label.component"
     ComponentCardComponent,
     LabelComponent,
     InputFieldComponent,
+    DropdownComponent,
   ],
   templateUrl: "./machine.component.html",
   styleUrl: "./machine.component.css",
 })
-export class MachineComponent {}
+export class MachineComponent {
+  isStatusOpen = false;
+  selectedStatus = "Active";
+
+  statusList = ["Active", "In Maintenance", "Out of Service", "Sold"];
+
+  toggleStatusDropdown() {
+    this.isStatusOpen = !this.isStatusOpen;
+  }
+
+  selectStatus(status: string) {
+    this.selectedStatus = status;
+    this.isStatusOpen = false;
+  }
+}
