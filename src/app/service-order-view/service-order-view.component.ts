3bf5127e9fb71e617829cfcbecb5135d220af5cf
MrNezihBJ <benjemianezih-glitch>
2025-10-14 02:36:37 +0100

    side bar added

--- src/app/service-order-view/service-order-view.component.ts
+++ src/app/service-order-view/service-order-view.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { AppSidebarComponent } from "../shared/layout/app-sidebar/app-sidebar.component";
 
 @Component({
   selector: 'app-service-order-view',
-  imports: [],
+  imports: [AppSidebarComponent],
   templateUrl: './service-order-view.component.html',
   styleUrl: './service-order-view.component.css'
 })
-export class ServiceOrderViewComponent {
+export class AppLayoutComponent {
+ 
+
+
+  
 
 }
