a678b07012eb0c751697a609a0e11ae48386a7ce
MrNezihBJ <benjemianezih-glitch>
2025-10-14 09:57:01 +0100

    removed the side bar issue

--- src/app/service-order-view/service-order-view.component.ts
+++ src/app/service-order-view/service-order-view.component.ts
@@ -1,16 +1,11 @@
-import { Component } from '@angular/core';
-import { AppSidebarComponent } from "../shared/layout/app-sidebar/app-sidebar.component";
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-service-order-view',
-  imports: [AppSidebarComponent],
+  imports: [],
   templateUrl: './service-order-view.component.html',
   styleUrl: './service-order-view.component.css'
 })
-export class AppLayoutComponent {
- 
-
-
-  
+export class ServiceOrderViewComponent {
 
 }
