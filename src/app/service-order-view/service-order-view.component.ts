186f5bb18d1d0bbeaa882b63017c4680806cdcb6
MrNezihBJ <benjemianezih-glitch>
2025-10-15 20:08:41 +0100

    will not work

--- src/app/service-order-view/service-order-view.component.ts
+++ src/app/service-order-view/service-order-view.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { CardOneComponent } from "../shared/components/cards/card-with-image/card-one/card-one.component";
+import { CardWithImageComponent } from "../shared/components/cards/card-with-image/card-with-image/card-with-image.component";
 
 @Component({
   selector: 'app-service-order-view',
   standalone: true, 
-  imports: [CardOneComponent],
+  imports: [CardOneComponent, CardWithImageComponent],
   templateUrl: './service-order-view.component.html',
   styleUrls: ['./service-order-view.component.css']
 })
