a90018eb73de521c3cefaaf9eef3340a29c2759a
Skander LASSOUED <lassoued.skanderr@gmail.com>
2025-11-19 23:43:31 +0100

    fix pages

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -2,10 +2,21 @@ import { Routes } from '@angular/router';
 import { Documentation } from './documentation/documentation';
 import { Crud } from './crud/crud';
 import { Empty } from './empty/empty';
+import { Article } from '@/article/article';
+import { Employee } from '@/employee/employee';
+import { Provider } from '@/provider/provider';
+import { Client } from '@/client/client';
+import { OrderService } from '@/order-service/order-service';
 
 export default [
     { path: 'documentation', component: Documentation },
     { path: 'crud', component: Crud },
+    { path: 'empty', component: Empty },
+    { path: 'article', component: Article },
+    { path: 'employee', component: Employee },
+    { path: 'provider', component: Provider },
+    { path: 'client', component: Client },
+    { path: 'order-service', component: OrderService },
     { path: 'empty', component: Empty },
     { path: '**', redirectTo: '/notfound' }
 ] as Routes;
